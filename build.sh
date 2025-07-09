b028d44f120f1c96f445ca161c14575dc7f4e54b
kmedyfa-dev <koustavamedya.2000@gmail.com>
2025-07-09 15:53:04 +0530

    Feat(CI):Integrate dynamic requirements

--- build.sh
+++ build.sh
@@ -7,6 +7,9 @@ PROFILE=${1:-default}
 echo "🔰 Buildozer Build Script Started"
 echo "📦 Using profile: $PROFILE"
 
+extra_reqs=$(paste -sd, /data/data/com.termux/files/home/webview_app/libytool-requirements.txt)
+sed -i "s/^requirements = .*/requirements = python,kivy,kivy-garden.webview,pysdl2,pyjnius,$extra_reqs/" buildozer.spec
+
 # The GitHub Actions workflow handles dependency installation and cleaning.
 # This script now focuses solely on executing the buildozer command.
 
